a29fe185232b7c7e3b6a1f71152a12cfca780fda
potato <kmn0848@naver.com>
2024-05-23 18:17:39 +0900

    템플릿 생성해서 데이터 출력

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,14 +1,24 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.ui.Model;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
+	
+	private final QuestionRepository questionRepository;
+	
 	@GetMapping("/question/list")
-	@ResponseBody
-	public String list() {
-		return "question List";
+	public String list(Model model) {
+		List<Question> qList = this.questionRepository.findAll();
+		model.addAttribute("questionList",qList);
+		
+		return "question_list";
 	}
 }
