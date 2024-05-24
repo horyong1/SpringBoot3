e25c385bc43ef98d537a8b55ad15fe2b9ce0e56f
potato <kmn0848@naver.com>
2024-05-24 22:24:57 +0900

    질문 목록에 링크 추가

--- sbb/src/main/java/com/mysite/sbb/question/QuestionServiec.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionServiec.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+
+// 데이터 처리 방식은 컨트롤러 -> 서비스 -> 리포지터리
+// 서비스단에서 모듈화하여 다른 컨트롤러에서도 필요한 메서드 사용 가능 
 @RequiredArgsConstructor
 @Service
 public class QuestionServiec {
@@ -14,7 +17,7 @@ public class QuestionServiec {
 	private final QuestionRepository questionRepository;
 	
 	public List<Question> getList(){
-		
+		// 앞에 컨트롤러에서 리포지터리 바로 접근하던 부분을 Service단에서 접근하여 데이터 처리
 		return this.questionRepository.findAll();
 	}
 }
