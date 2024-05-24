0857fd7cb775bc7161683d4e33cc539d120ca14b
potato <kmn0848@naver.com>
2024-05-24 22:44:54 +0900

    id 값으로 데이터 조회 메서드 생성

--- sbb/src/main/java/com/mysite/sbb/question/QuestionServiec.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionServiec.java
@@ -1,8 +1,11 @@
 package com.mysite.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,5 +22,15 @@ public class QuestionServiec {
 	public List<Question> getList(){
 		// 앞에 컨트롤러에서 리포지터리 바로 접근하던 부분을 Service단에서 접근하여 데이터 처리
 		return this.questionRepository.findAll();
+	}
+	
+	public Question getQuestion(Integer id) {
+			Optional<Question> question = this.questionRepository.findById(id);
+			if(question.isPresent()) {
+				return question.get();
+			} else {
+				throw new DataNotFoundException("question not found");
+			}
+		 
 	}
 }
