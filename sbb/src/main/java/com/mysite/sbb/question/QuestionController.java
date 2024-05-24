fe0ca2aff319c728cfa9a53ecc898babd6d1302d
potato <kmn0848@naver.com>
2024-05-24 22:45:09 +0900

    id 값으로 데이터 조회

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -26,7 +26,8 @@ public class QuestionController {
 	
 	@GetMapping(value = "/question/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
-	
+		Question question = this.questionServiec.getQuestion(id);
+		model.addAttribute("question", question);
 		return "question_detail";
 	}
 }
